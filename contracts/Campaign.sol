dd5f690537f9473912d652dddc31d8ae56f26bcf
xkw1602 <xkw.1602@gmail.com>
2025-04-30 23:51:28 -0500

    Updated milestone logic to be defined at campaign construction

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -2,12 +2,14 @@
 pragma solidity ^0.8.28;
 
 contract Campaign {
+    // Milestsones wil be defined at construction with a percentage of how much to release upon approval
     struct Milestone {
         string description;
-        uint amount;
+        uint percentage;
         bool approved;
         uint approvalCount;
         mapping(address => bool) approvals;
+        bool votingActive;
     }
 
     address public creator;
@@ -25,12 +27,30 @@ contract Campaign {
         _;
     }
 
-    constructor(address _creator, uint _goal, string memory _title) {
+    // All milestones are created at once along with the campaign
+    constructor(address _creator, uint _goal, string memory _title, string[] memory _descriptions, uint[] memory _percentages) {
+        require(_percentages.length == _descriptions.length, "Mismatched lengths");
+
+        uint totalPercentage;
         creator = _creator;
         fundingGoal = _goal;
         title = _title;
+        for (uint i = 0; i < _descriptions.length; i++) {
+            Milestone storage m = milestones[i];
+            m.description = _descriptions[i];
+            m.percentage = _percentages[i];
+            m.approved = false;
+            m.approvalCount = 0;
+            m.votingActive = false;
+            totalPercentage += m.percentage;
+            milestoneCount++;
+        }
+
+        // Check that the total percentage of all milestones is 100
+        require(totalPercentage == 100, "Total percentage must be 100");
     }
 
+    // Contributors can contribute to the campagin as a whole
     function contribute() external payable {
         require(msg.value > 0, "Must send ETH > 0");
         if (contributions[msg.sender] == 0) {
@@ -40,25 +60,33 @@ contract Campaign {
         totalRaised += msg.value;
     }
 
-    function createMilestone(string calldata desc, uint amount) external onlyCreator {
-        require(amount <= totalRaised, "Insufficient funds");
-        Milestone storage m = milestones[milestoneCount++];
-        m.description = desc;
-        m.amount = amount;
-    }
 
+    // Once a milestone is activated, contributors can vote on it
+    // If a milestone is approved, the funds are released to the creator
     function voteOnMilestone(uint id) external {
         Milestone storage m = milestones[id];
+        require(m.votingActive, "Milestone not votable yet");
         require(contributions[msg.sender] > 0, "Not a contributor");
         require(!m.approvals[msg.sender], "Already voted");
+        require(msg.sender != creator, "Creator cannot vote");
+        require(!m.approved, "Milestone already approved");
 
         m.approvals[msg.sender] = true;
         m.approvalCount++;
 
+        // 50%+ approval needed to approve
         if (m.approvalCount > contributors.length / 2) {
             m.approved = true;
-            payable(creator).transfer(m.amount);
+            payable(creator).transfer(m.percentage * totalRaised / 100);
         }
+    }
+
+    // Creator activates a milestone when they are ready to have contributors vote on it
+    function activateMilestone(uint id) external onlyCreator {
+        Milestone storage m = milestones[id];
+        require(!m.votingActive, "Milestone already active");
+
+        m.votingActive = true;
     }
 
     function refund() external {
